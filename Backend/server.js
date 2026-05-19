e803ed32a4a2686b57fb46664dca48e93a5750b4
zishanx <zishanalikhan53@gmail.com>
2026-05-19 22:33:41 +0530

    Checked everything is working fine the auth is done

--- Backend/server.js
+++ Backend/server.js
@@ -14,6 +14,6 @@ app.use('/api',authRouter)
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log(`MongoDb Connected`);
-        app.listen(process.env.PORT,()=>{console.log(`Server is runnng on port ${process.env.PORT}`)})
+        app.listen(process.env.PORT,()=>{console.log(`Server is running on port ${process.env.PORT}`)})
     })
     .catch(err => console.log(err))
