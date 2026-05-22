173c3e624aa06bf848e3d89c50809f08b03df4bf
zishanx <zishanalikhan53@gmail.com>
2026-05-22 21:45:07 +0530

    Dashboard checked

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,9 @@ import Register from "./pages/regsiter"
 import Home from './pages/home'
 import { BrowserRouter, Routes, Route } from "react-router"
 import { AuthProvider } from "./contexts/Authcontext"
+import ProtectedRoute from "./components/protectedroute"
+
+import Dashboard from "./pages/dashboard"
 
 export default function App() {
   return (
@@ -12,6 +15,7 @@ export default function App() {
           <Route path="/" element={<Home />}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/register" element={<Register />}></Route>
+          <Route path="/dashboard" element={<ProtectedRoute><Dashboard/></ProtectedRoute>}></Route>
         </Routes>
       </AuthProvider>
     </BrowserRouter>
